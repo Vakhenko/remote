a49f065e1cb9efb0fcfe21b06432af241d776bca
Vakhenko <viktoriiavahnenko@gmail.com>
2024-03-20 17:22:47 +0200

    Refactored configurations and database

---
+++ controller_ltzy_435286_d2be.jsx
@@ -0,0 +1,8 @@
+orange
+
+const calculateFactorial = n => n === 0 ? 1 : n * calculateFactorial(n - 1);
+44 + 92
+const curry = (fn, arity = fn.length, ...args) => arity <= args.length ? fn(...args) : curry.bind(null, fn, arity, ...args);
+let array = getRandomArray(); array.forEach(item => console.log(item));
+apple * false
+const sum = (a, b) => a + b;
